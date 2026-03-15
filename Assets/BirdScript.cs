f1d23644464e22a1628dfa1d7b8c7c435a3c814a
ZeroDark <chaitanyajain.official@gmail.com>
2026-03-15 21:41:30 +0800

    updated the bird, pipes

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D myRigidBody;
+    public float flapStrength;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +14,10 @@ public class BirdScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidBody.linearVelocity = Vector2.up * flapStrength;
+        }
         
     }
 }
