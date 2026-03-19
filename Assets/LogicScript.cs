1a6c432b4eaa45266aee6cda92bf6468606d4a9a
ZeroDark <chaitanyajain.official@gmail.com>
2026-03-19 19:15:10 +0800

    some modification

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -1,14 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 public class LogicScript : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public int playerScore;
     public Text ScoreText;
+    public GameObject gameOverScreen;
+    
+    
     [ContextMenu("Add Score")]
     public void addScore(int scoreToAdd)
     {
         playerScore += scoreToAdd;
         ScoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
--- Assets/PipeMoveScript.cs
+++ Assets/PipeMoveScript.cs
@@ -14,7 +14,7 @@ public class PipeMoveScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime;
+        transform.position = transform.position + (Vector3.left * moveSpeed) * Time.deltaTime * 2;
         if (transform.position.x < DeadZone)
         {
             // Debug.Log("Pipe Deleted");
